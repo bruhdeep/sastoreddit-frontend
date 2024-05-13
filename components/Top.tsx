00702e18a020a986df229c8200b3e24b3f9794cb
welpe-e <haleluya3479@gmail.com>
2024-05-14 00:20:52 +0545

    Fixed Top Forums

--- components/Top.tsx
+++ components/Top.tsx
@@ -1,24 +1,14 @@
 import { useEffect, useState } from "react";
 
-import TopBloggers from "./TopBloggers";
-import TopForums from "./TopForums";
-
 interface Forum {
   name: string;
   description: string;
   popularity: number;
 }
 
-interface Blogger {
-  userName: string;
-  popularity: number;
-}
-
 export default function Top() {
   const [month, setMonth] = useState<number | null>(null);
   const [topForums, setTopForums] = useState<Forum[]>([]);
-  const [topBloggers, setTopBloggers] = useState<Blogger[]>([]);
-  const [activeTab, setActiveTab] = useState<"forums" | "bloggers">("forums");
 
   useEffect(() => {
     const fetchTopForums = async () => {
@@ -26,38 +16,17 @@ export default function Top() {
       if (month) {
         url += `?month=${month}`;
       }
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "ngrok-skip-browser-warning": "1",
-        },
-      });
-      const data = await response.json();
-      if (typeof data === "string") {
-        console.log(data);
-        setTopForums([]);
-      } else {
+      const response = await fetch(url);
+      const text = await response.text();
+      try {
+        const data = JSON.parse(text);
         setTopForums(data);
+      } catch {
+        setTopForums([]);
       }
-    };
-
-    const fetchTopBloggers = async () => {
-      let url = process.env.BASE_URL + "/admin/top-bloggers";
-      if (month) {
-        url += `?month=${month}`;
-      }
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "ngrok-skip-browser-warning": "1",
-        },
-      });
-      const data = await response.json();
-      setTopBloggers(data);
     };
 
     fetchTopForums();
-    fetchTopBloggers();
   }, [month]);
 
   const handleNumberChange = (selectedNumber: number) => {
@@ -66,8 +35,8 @@ export default function Top() {
 
   return (
     <div className="flex flex-col items-center">
-      <div className="mt-4 flex justify-between w-[70%]">
-        <div className="flex space-x-4">
+      <div className="mt-4 flex justify-center w-[70%]">
+        <div className="flex space-x-6">
           {Array.from({ length: 12 }, (_, i) => i + 1).map((number) => (
             <button
               key={number}
@@ -80,17 +49,74 @@ export default function Top() {
             </button>
           ))}
         </div>
-        <button
-          className={`btn text-black ${
-            activeTab === "bloggers" ? "bg-indigo-300" : "bg-white"
-          } hover:bg-indigo-300`}
-          onClick={() => setActiveTab("bloggers")}
-        >
-          Top Bloggers
-        </button>
       </div>
-      {activeTab === "forums" && <TopForums topForums={topForums} />}
-      {activeTab === "bloggers" && <TopBloggers topBloggers={topBloggers} />}
+      {topForums.length === 0 ? (
+        <div className="mt-10">
+          <p>No activity this month.</p>
+        </div>
+      ) : (
+        <div className="mt-10">
+          <div className="grid grid-cols-2 gap-36">
+            {/* Displaying the first five forums */}
+            <div>
+              {topForums.slice(0, 5).map((forum, index) => (
+                <div
+                  key={index}
+                  className="flex flex-row space-y-4 items-center w-full"
+                >
+                  <p className="text-2xl font-semibold flex items-center px-2 mr-4">
+                    {index === 0 ? (
+                      <span className="text-2xl">1</span>
+                    ) : (
+                      index + 1
+                    )}
+                    .
+                  </p>
+                  <ul className="space-y-2 w-full">
+                    <li
+                      className={`bg-gray-100 rounded-lg p-4 ${
+                        index === 0 ? "shadow-glow" : ""
+                      }`}
+                    >
+                      <p className="font-bold text-lg">{forum.name}</p>
+                      <p className="text-sm text-gray-600">
+                        Description: {forum.description}
+                      </p>
+                      <p className="text-sm text-gray-600">
+                        Popularity: {forum.popularity}
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              ))}
+            </div>
+            {/* Displaying the next five forums */}
+            <div>
+              {topForums.slice(5).map((forum, index) => (
+                <div
+                  key={index}
+                  className="flex flex-row space-y-4 items-center w-full "
+                >
+                  <p className="text-2xl font-semibold flex items-center px-2 mr-4">
+                    {index + 6}.
+                  </p>
+                  <ul className="space-y-2 w-full">
+                    <li className="bg-gray-100 rounded-lg p-4">
+                      <p className="font-bold text-lg">{forum.name}</p>
+                      <p className="text-sm text-gray-600">
+                        Description: {forum.description}
+                      </p>
+                      <p className="text-sm text-gray-600">
+                        Popularity: {forum.popularity}
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
